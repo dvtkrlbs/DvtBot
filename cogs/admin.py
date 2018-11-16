eece71e2627d22a52a565c7722f91e9f262275df
Tunahan Karlıbaş <dvt.tnhn.krlbs@gmail.com>
2018-11-16 10:45:06 +0300

    Add owner only eval command

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,14 +1,32 @@
-import discord
 from discord.ext import commands
+import asyncio
 import traceback
+import discord
+import inspect
+import textwrap
+from contextlib import redirect_stdout
+import io
+import copy
+from typing import Union
 import subprocess
 
 class AdminCog:
     def __init__(self, bot):
+        self._last_result = None
         self.bot = bot
 
     async def __local_check(self, ctx):
         return await self.bot.is_owner(ctx.author)
+
+    def cleanup_code(self, content):
+        """Automatically removes code blocks from the code."""
+        # remove ```py\n```
+        if content.startswith('```') and content.endswith('```'):
+            return '\n'.join(content.split('\n')[1:-1])
+
+        # remove `foo`
+        return content.strip('` \n')
+
 
     @commands.command(hidden=True)
     async def load(self, ctx, module):
@@ -49,7 +67,54 @@ class AdminCog:
                 await ctx.send(f'```py\n{traceback.format_exc()}\n```')
         else:
             await ctx.send('\N{OK HAND SIGN}')
-            
+    
+
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    async def _eval(self, ctx, *, body: str):
+        """Evaluates a code"""
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
 
 
 def setup(bot):
